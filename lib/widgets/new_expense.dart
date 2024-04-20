70fed1a84b17b7c19bd3a699959f6436df29f4c3
Abdullah Majeed <abdullahmajeed969@gmail.com>
2024-04-20 17:25:43 +0500

    modal working

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: titleController,
+            maxLength: 50,
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          TextField(
+            controller: amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amount'),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(amountController.text);
+                },
+                child: const Text('Save expense'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
